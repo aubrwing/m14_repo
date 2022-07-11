cc08bd1f8dc82798d8c313e51bad2ad7feb542a1
aubrwing <alw8ef@virginia.edu>
2022-07-11 16:01:14 -0400

    M014 In Class Activity

---
+++ demo_main/demo/printFunctionWorking.py
@@ -0,0 +1,7 @@
+def printFunctionWorking():
+    """
+    Inputs: None
+    Outputs: A string that says "This function is working"
+    Purpose: Showing that we know how to make a package in python
+    """
+    print("This function is working")
---
+++ demo_main/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='Demo',
+      version='0.1',
+      description='A pointless demonstration package',
+      url='https://github.com/aubrwing/m14_repo',
+      author='Aubrey Winger',
+      author_email='alw8ef@virginia.edu',
+      license='',
+      packages=['demo'])
+
